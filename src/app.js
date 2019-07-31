4a80f2049b1b454ec9b86a37fa27d25f319e5b84
jasoncarcamo <jasoncarcamo30@yahoo.com>
2019-07-31 15:11:42 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
-const morgan = require('morgan');
 const folderRouter = require('./folders/folderRouter');
 const notesRouter = require('./notes/notesRouter');
 
 
 app.use(cors());
 app.use(helmet());
-app.use(morgan('common'));
 
 app.use('/folders', folderRouter);
 app.use('/notes', notesRouter);
