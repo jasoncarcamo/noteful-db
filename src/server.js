ab27565544c39831c09cdcbf5f4837a908849a6f
jasoncarcamo <jasoncarcamo30@yahoo.com>
2019-07-31 14:51:44 -0400

    ready for deployment

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const app = require('./app');
 const knex = require('knex');
+const PORT = process.env.PORT || 8000;
 
 const db = knex({
     client: 'pg',
-    connection: process.env.DB_URL
+    connection: process.env.DATABASE_URL
 })
 
 app.set('db', db);
 
-app.listen(8000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Listening on http://localhost:8000');
 })
